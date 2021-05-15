cc3c77b770e3b5bc3f08262ddee525045836084f
dallahdev123 <toureabdoullah2552@gmail.com>
2021-05-15 02:25:01 +0000

    message d'erreur

---
+++ src/app/creer-compte/creer-compte.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { CreerCompteService } from '../Services/creer-compte.service'
+import {FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './creer-compte.component.html',
+  styleUrls: ['./creer-compte.component.css']
+})
+export class CreerCompteComponent{
+  registerUserData = {
+    nom:"nom",
+    prenom: "prenom",
+    email : "email",
+    profil : 'profil'
+    
+  };
+  email = new FormControl('', [Validators.required,
+     Validators.email]);
+  constructor(private _auth: CreerCompteService) { }
+
+  registerUser(){
+    this._auth.registerUser(this.registerUserData)
+    .subscribe(
+      (res: any) => console.log(res),
+      (err: any) => console.log(err)
+    )
+  }
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Ce champs ne peut pas être vide';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  
+}
