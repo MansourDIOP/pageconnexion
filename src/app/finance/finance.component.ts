ea09682dba11ee41d83b12e24afe1ffe47e7b8ed
cheikh885 <dcheikhballa9@gmail.com>
2021-05-16 22:41:00 +0200

    page etudiant et finance

--- src/app/finance/finance.component.ts
+++ src/app/finance/finance.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./finance.component.css']
 })
 export class FinanceComponent implements OnInit {
+  monimage2:string = "assetsfinance2.jpg";
+  monimage1:string = "assets/images/finance1.png";
+  monimage4:string = "assets/Orange_Money-Logo.wine.png";
+  monimage3:string = "assets/Budget3.png";
 
   constructor() { }
 
