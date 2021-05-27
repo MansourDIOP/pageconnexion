dab437d1dae934e18612af366b1fe3625d9d5afa
dallahdev123 <toureabdoullah2552@gmail.com>
2021-05-27 13:36:30 +0000

    page formateur

--- src/app/mot-de-passe-oublie/mot-de-passe-oublie.component.ts
+++ src/app/mot-de-passe-oublie/mot-de-passe-oublie.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import { AuthService } from '../Services/auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-mot-de-passe-oublie',
@@ -10,7 +12,7 @@ export class MotDePasseOublieComponent implements OnInit {
   registerUserData = {
    email : "mindset@example.com",    
   };
-  constructor() { }
+  constructor(private _auth : AuthService, private _router: Router) { }
 
   ngOnInit(): void {
   }
@@ -23,5 +25,17 @@ export class MotDePasseOublieComponent implements OnInit {
       }
   
       return this.email.hasError('email') ? 'Not a valid email' : '';
+    }
+
+    forget(){
+      this._auth.forgetPassword(this.registerUserData)
+      .subscribe(
+        (res: any) => {
+          console.log(res)
+          localStorage.setItem('token', res.token)
+          this._router.navigate(['/'])
+        },
+        (err: any) => console.log(err)
+      )
     }
 }
